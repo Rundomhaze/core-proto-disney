0a9239475f945fffe5ea2f05b4e61fefbc37d88d
kayday <kadaniilo@gmail.com>
2020-03-12 11:41:16 +0300

    Adds tests for cartoons

--- solution/cartoon.js
+++ solution/cartoon.js
@@ -4,7 +4,7 @@
  * @param {*} year 
  * @param {*} forChildren 
  */
-const Cartoon = function (title, year, forChildren) {
+const Cartoon = function (title, year, forChildren=true) {
   this.title = title;
   this.year = year;
   this.forChildren = true;
@@ -25,7 +25,7 @@ Cartoon.prototype.getDescription = function () {
 Cartoon.prototype.play = function () {
   
   return [
-    ' '.repeat(Math.floor(this.studio.length/3)) + this.logo,
+    ' '.repeat(Math.floor(this.studio.length/3)) + this.studioLogo,
     this.studio,
   '-'.repeat(this.studio.length + 4),
     this.title,
@@ -44,7 +44,7 @@ Cartoon.prototype.play = function () {
 const DisneyCartoon = function (title, year) {
   Cartoon.apply(this, arguments); // Вызов родительского конструктора
   this.studio = 'Walt Disney';
-  this.logo = '🏰🌠';
+  this.studioLogo = '🏰🌠';
   this.beginning = 'Long, long ago in a faraway land...';
 }
 
@@ -52,17 +52,6 @@ const DisneyCartoon = function (title, year) {
 DisneyCartoon.prototype = Object.create(Cartoon.prototype); // new Cartoon();
 DisneyCartoon.prototype.constructor = DisneyCartoon;
 
-// DisneyCartoon.prototype.play = function () {
-//   return `
-//       ${this.logo}
-//   ${this.studio}
-// ${'-'.repeat(this.studio.length + 4)}
-  
-//   ${this.title}
-
-// ${'-'.repeat(this.title.length + 4)}
-//   ${this.beginning}`;
-// }
 
 
 /**
@@ -72,9 +61,9 @@ DisneyCartoon.prototype.constructor = DisneyCartoon;
  */
 const DreamWorksCartoon = function (title, year) {
   Cartoon.apply(this, arguments); // Вызов родительского конструктора
-  this.forChildren = false;
+  this.forChildren = true;
   this.studio = 'DreamWorks';
-  this.logo = '🎣🌙';
+  this.studioLogo = '🌙';
   this.beginning = 'Long, long ago in a faraway land...';
 }
 
@@ -82,18 +71,8 @@ const DreamWorksCartoon = function (title, year) {
 DreamWorksCartoon.prototype = Object.create(Cartoon.prototype); // new Cartoon();
 DreamWorksCartoon.prototype.constructor = DreamWorksCartoon;
 
-// DreamWorksCartoon.prototype.play = function () {
-  
-//   return [
-//     ' '.repeat(Math.floor(this.studio.length/3)) + this.logo,
-//     this.studio,
-//   '-'.repeat(this.studio.length + 4),
-//     this.title,
-//   '-'.repeat(this.title.length + 4),
-//     this.beginning,
-//   ].join('\n');
-// }
 
+// -----------------------
 
 
 const theLittleMermaid = new DisneyCartoon('The Little Mermaid 🧜', 1989);
@@ -106,7 +85,12 @@ console.log(
 
 
 
-const kungFuPanda = new DreamWorksCartoon('Kung Fu Panda 🐼', 2008);
+const kungFuPanda = new DreamWorksCartoon(
+  'Kung Fu Panda 🐼', 
+  2008,
+  'Legend tells of a legendary warrior whose Kung Fu skills were the stuff of legend...'
+  );
+
 kungFuPanda.beginning = 'Legend tells of a legendary warrior whose Kung Fu skills were the stuff of legend...';
 
 // console.log(kungFuPanda);
--- solution/spec/cartoon-spec.js
+++ solution/spec/cartoon-spec.js
@@ -1,178 +1,172 @@
-// BEHAVIORAL SPECS
-describe('Cartoon', function() {
-  'use strict';
+describe('Cartoon', () => {
 
-  describe('the constructor', function() {
+  describe('the constructor', () => {
 
-    it('is a function', function() {
-      expect(Vehicle).toEqual(jasmine.any(Function));
+    it('is a function', () => {
+      expect(Cartoon).toEqual(jasmine.any(Function));
     });
 
-    describe('when called', function() {
-      var vehicle;
-      beforeEach(function() {
-        vehicle = new Vehicle('bob');
+    describe('when called', () => {
+
+      it('sets title, year, age restrictions #1', () => {
+        let cartoon = new Cartoon('Малыш и Карлсон', 1968);
+        expect(cartoon.title).toEqual('Малыш и Карлсон');
+        expect(cartoon.year).toEqual(1968);
+        expect(cartoon.forChildren).toBeTruthy();
       });
 
-      it('sets the driver', function() {
-        expect(vehicle.driver).toEqual('bob');
+      it('sets title, year, age restrictions #2', () => {
+        let cartoon = new Cartoon('Corpse Bride', 2005, false);
+        expect(cartoon.title).toEqual('Corpse Bride');
+        expect(cartoon.year).toEqual(2005);
+        expect(cartoon.forChildren).toBeFalsy();
       });
 
-      it('sets the initial speed', function() {
-        expect(vehicle.speed).toEqual(0);
-      });
     });
+
   });
 
-  describe('#drive', function() {
-    var vehicle;
-    beforeEach(function() {
-      vehicle = new Vehicle('bob');
-    });
-
-    it('exists', function() {
-      expect(vehicle.drive).toEqual(jasmine.any(Function));
-    });
-
-    it('drives', function() {
-      expect(vehicle.drive(20)).toEqual('bob driving at 20 miles per hour');
-    });
-
-    it('sets the speed', function() {
-      vehicle.drive(45);
-      expect(vehicle.speed).toEqual(45);
-    });
-  });
-});
-
-describe('Car', function() {
-  'use strict';
-
-  describe('the constructor', function() {
-
-    it('is a function', function() {
-      expect(Car).toEqual(jasmine.any(Function));
-    });
-
-    describe('when called', function() {
-      var car;
-      beforeEach(function() {
-        car = new Car('bob');
-      });
-
-      it('sets the driver', function() {
-        expect(car.driver).toEqual('bob');
-      });
-
-      it('sets the initial speed', function() {
-        expect(car.speed).toEqual(0);
-      });
-    });
-  });
-
-  describe('#drive', function() {
-    var car;
-    beforeEach(function() {
-      car = new Car('bob');
-    });
-
-    it('exists', function() {
-      expect(car.drive).toEqual(jasmine.any(Function));
-    });
-
-    it('drives', function() {
-      expect(car.drive(20)).toEqual('bob driving at 20 miles per hour');
-    });
-
-    it('sets the speed', function() {
-      car.drive(45);
-      expect(car.speed).toEqual(45);
-    });
-  });
 });
 
 
-describe('Truck', function() {
-  'use strict';
+describe('Disney Cartoon', () => {
 
-  describe('the constructor', function() {
+  describe('the constructor', () => {
 
-    it('is a function', function() {
-      expect(Truck).toEqual(jasmine.any(Function));
+    it('is a function', () => {
+      expect(DisneyCartoon).toEqual(jasmine.any(Function));
     });
 
-    describe('when called', function() {
-      var truck;
-      beforeEach(function() {
-        truck = new Truck('bob');
+    it('extends Cartoon', () => {
+      expect(DisneyCartoon.prototype).toEqual(Object.create(Cartoon)); // props
+    });
+
+    it('extends Cartoon but the constructor remains the same', () => {
+      expect(DisneyCartoon.prototype.constructor).toEqual(DisneyCartoon);
+    });
+
+    describe('when called', () => {
+
+      let disneyCartoon;
+
+      beforeEach(() => {
+        disneyCartoon = new DisneyCartoon('The Little Mermaid 🧜', 1989);
       });
 
-      it('sets the driver', function() {
-        expect(truck.driver).toEqual('bob');
+      it('has Cartoon`s properties', () => {
+        expect(disneyCartoon.title).toEqual('The Little Mermaid 🧜');
+        expect(disneyCartoon.year).toEqual(1989);
+        expect(disneyCartoon.forChildren).toBeTruthy();
       });
 
-      it('sets the initial speed', function() {
-        expect(truck.speed).toEqual(0);
+      it('has it`s own properties', () => {
+        expect(disneyCartoon.studio).toEqual('Walt Disney');
+        expect(disneyCartoon.studioLogo).toEqual('🏰🌠');
+        expect(disneyCartoon.beginning).toEqual('Long, long ago in a faraway land...'); // default beginning.
       });
 
-      it('sets the initial cargo load', function() {
-        expect(truck.cargo).toEqual([]);
+    });
+
+    describe('.play', () => {
+
+      let disneyCartoon;
+
+      beforeEach(() => {
+        disneyCartoon = new DisneyCartoon(
+          'The Lion King 🦁',
+          1994,
+          true,
+          'Naaaaaaaaants ingonyaaaaaaama bagithi Baba...',
+        );
       });
+
+      it('contains the studio and it`s logo', () => {
+        expect(disneyCartoon.play()).toContain(disneyCartoon.studio);
+        expect(disneyCartoon.play()).toContain(disneyCartoon.studioLogo);
+      });
+
+      it('contains the cartoon`s title', () => {
+        expect(disneyCartoon.play()).toContain(disneyCartoon.title);
+      });
+
+      it('contains the beginning of the cartoon', () => {
+        expect(disneyCartoon.beginning).toBe('Naaaaaaaaants ingonyaaaaaaama bagithi Baba...');
+        expect(disneyCartoon.play()).toContain(disneyCartoon.beginning);
+      });
+
     });
+
   });
 
-  describe('#drive', function() {
-    var truck;
-    beforeEach(function() {
-      truck = new Truck('bob');
+});
+
+
+describe('DreamWorks Cartoon', () => {
+
+  describe('the constructor', () => {
+
+    it('is a function', () => {
+      expect(DreamWorksCartoon).toEqual(jasmine.any(Function));
     });
 
-    it('exists', function() {
-      expect(truck.drive).toEqual(jasmine.any(Function));
+    it('extends Cartoon', () => {
+      expect(DreamWorksCartoon.prototype).toEqual(Object.create(Cartoon)); // props
     });
 
-    it('drives', function() {
-      expect(truck.drive(20)).toEqual('bob driving at 20 miles per hour');
+    it('extends Cartoon but the constructor remains the same', () => {
+      expect(DreamWorksCartoon.prototype.constructor).toEqual(DreamWorksCartoon);
     });
 
-    it('sets the speed', function() {
-      truck.drive(45);
-      expect(truck.speed).toEqual(45);
+    describe('when called', () => {
+      let dreamWorksCartoon;
+
+      beforeEach(() => {
+        dreamWorksCartoon = new DreamWorksCartoon(
+          'Kung Fu Panda 🐼',
+          2008,
+          true,
+          'Legend tells of a legendary warrior whose Kung Fu skills were the stuff of legend...',
+        );
+      });
+
+      it('has Cartoon`s properties', () => {
+        expect(dreamWorksCartoon.title).toEqual('Kung Fu Panda 🐼');
+        expect(dreamWorksCartoon.year).toEqual(2008);
+        expect(dreamWorksCartoon.forChildren).toBeTruthy();
+      });
+
+      it('has it`s own properties', () => {
+        expect(dreamWorksCartoon.studio).toEqual('DreamWorks');
+        expect(dreamWorksCartoon.studioLogo).toEqual('🌙');
+        expect(dreamWorksCartoon.beginning)
+          .toEqual('Legend tells of a legendary warrior whose Kung Fu skills were the stuff of legend...');
+      });
+
     });
+
+    describe('.play', () => {
+
+      let dreamWorksCartoon;
+
+      beforeEach(() => {
+        dreamWorksCartoon = new DreamWorksCartoon('Shrek 💚', 2001);
+      });
+
+      it('contains the studio and it`s logo', () => {
+        expect(dreamWorksCartoon.play()).toContain(dreamWorksCartoon.studio);
+        expect(dreamWorksCartoon.play()).toContain(dreamWorksCartoon.studioLogo);
+      });
+
+      it('contains the cartoon`s title', () => {
+        expect(dreamWorksCartoon.play()).toContain(dreamWorksCartoon.title);
+      });
+
+      it('contains the beginning of the cartoon', () => {
+        expect(dreamWorksCartoon.play()).toContain(dreamWorksCartoon.beginning);
+      });
+
+    });
+
   });
 
-  describe('#loadCargo', function() {
-    var truck;
-    beforeEach(function() {
-      truck = new Truck('bob');
-      debugger;
-      truck.loadCargo('test cargo');
-    });
-
-    it('adds the cargo on the truck', function() {
-      expect(truck.cargo).toEqual(['test cargo']);
-    });
-
-    it('returns itself', function() {
-      expect(truck.loadCargo('test')).toBe(truck);
-    });
-  });
-
-  describe('#unloadCargo', function() {
-    var truck,
-        unloadedCargo;
-    beforeEach(function() {
-      truck = new Truck('bob');
-      truck.cargo = ['cargo 1', 'cargo 2'];
-      unloadedCargo = truck.unloadCargo();
-    });
-
-    it('returns the latest cargo', function() {
-      expect(unloadedCargo).toEqual('cargo 2');
-    });
-
-    it('reduces the cargo load by 1', function() {
-      expect(truck.cargo.length).toEqual(1);
-    });
-  });
 });
