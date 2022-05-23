6b3779cbad35c9b5b66c1979dba92f065c0de45b
Alex <petenkov2822@gmail.com>
2022-05-23 18:51:51 +0400

    Releases 0-1-2

--- src/cartoon.js
+++ src/cartoon.js
@@ -21,27 +21,30 @@ Cartoon.prototype.play = function () {
     this.studio,
     this.title,
     this.beginning,
+    this.studioLogo
   ].join('\n');
 };
 
 /**
  * Мульт студии Walt Disney.
  */
-function DisneyCartoon(title, year, forChildren, beginning) {
-  this.title = title;
-  this.year = year;
-  this.beginning = beginning;
+function DisneyCartoon(title, year, forChildren, beginning = 'Long, long ago in a faraway land...') {
+  Cartoon.call(this, title, year, forChildren, beginning)
   this.studio = 'Walt Disney';
+  this.studioLogo = '🏰🌠'
 }
+
 
 /**
  * Мульт студии DreamWorks.
  */
 function DreamWorksCartoon(title, year, forChildren, beginning) {
-  this.title = title;
-  this.year = year;
-  this.forChildren = forChildren;
+  Cartoon.call(this, title, year, forChildren, beginning)
   this.studio = 'DreamWorks';
+  this.studioLogo = '🌙'
 }
+
+Object.setPrototypeOf(DisneyCartoon.prototype, Cartoon.prototype);
+Object.setPrototypeOf(DreamWorksCartoon.prototype, Cartoon.prototype);
 
 module.exports = { Cartoon, DisneyCartoon, DreamWorksCartoon };
--- src/person.js
+++ src/person.js
@@ -2,12 +2,27 @@
  * Человек.
  * @param {string} name
  */
-function Person(name) {
-  // Код тут.
-}
+
+const {
+  Cartoon,
+  DisneyCartoon,
+  DreamWorksCartoon }
+  = require('../src/cartoon')
+
+function Person(name, watchedMovies) {
+  this.name = name;
+  this.watchedMovies = watchedMovies;
+  this.watchedMovies.toString = function () {
+    return watchedMovies.map((el, ind) => `${ind + 1} ${el.title}`)
+      .join('\n');
+  }
+};
 
 Person.prototype.watchMovie = function (movie) {
-  // Код тут.
+  movie.play();
+  if (!this.watchedMovies.includes(movie)) {
+    this.watchedMovies.push(movie)
+  };
 };
 
 module.exports = { Person };
